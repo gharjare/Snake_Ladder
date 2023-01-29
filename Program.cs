f3791148157aea3ef60d01295e9b47c892ae681a
gharjare <gharjarechetan@gmail.com>
2023-01-29 16:42:28 +0530

    [chetan gharjare] start game with single player at zero position

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake ladder problem");
-
+            int position = 0;
+            int player = 1;
+            Console.WriteLine("Position" + position);
         }
     }
 }
